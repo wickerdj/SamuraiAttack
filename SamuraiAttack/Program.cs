9502c60fdd0c3cfd94756eb74b38aafefb0ee055
wick <wickerdj@gmail.com>
2015-02-22 16:32:11 -0600

    Init Checking

---
+++ SamuraiAttack/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Ninject;
+using Ninject.Modules;
+
+namespace SamuraiAttack
+{
+
+
+
+
+	public interface IWeapon {
+		void Hit (string target);
+	}
+
+	public class Sword : IWeapon {
+		public void Hit(String target) {
+			Console.WriteLine ("Chopped {0} clean in half.", target);
+		}
+	}
+
+	public class Shuriken : IWeapon{
+		public void Hit(string target) {
+			Console.WriteLine ("Pierces the {0}'s armor.", target);
+		}
+	}
+
+
+	public interface IWarrior{
+		void Attack (string target);
+	}
+
+	public class Samurai: IWarrior {
+		private readonly IWeapon _weapon;
+
+		public Samurai(IWeapon weapon) {
+			_weapon = weapon;
+		}
+
+		public void Attack(string target) {
+			_weapon.Hit (target);
+		}
+	}
+
+	public class WarriorModule:NinjectModule {
+		public override void Load() {
+			Bind<IWarrior> ().To<Samurai> ();
+			Bind<IWeapon> ().To<Sword> ();
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			IKernel kernel = new StandardKernel (new WarriorModule());
+
+			IWarrior samurai = kernel.Get<IWarrior>();
+
+			samurai.Attack ("bad programmers");
+			Console.ReadLine ();
+		}
+	}
+}
